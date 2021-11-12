f299759aecf2a375cff26c93d9af47fab2dedc6a
Ruslan <ruslan.shtybaev@gmail.com>
2021-11-12 19:31:50 +0300

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Руслан Штыбаев on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
